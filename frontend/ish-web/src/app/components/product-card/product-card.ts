11d355c368bbd31dda9688204b05cccd12efec53
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-06-12 15:09:19 +0000

    Refactor: add ProductCard component and use @Input

--- frontend/ish-web/src/app/components/product-card/product-card.ts
+++ frontend/ish-web/src/app/components/product-card/product-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.scss'
 })
 export class ProductCard {
-
+  @Input() product!: Product;
 }
--- frontend/ish-web/src/app/pages/products/products.ts
+++ frontend/ish-web/src/app/pages/products/products.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
+import { ProductCard } from '../../components/product-card/product-card';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductCard],
   templateUrl: './products.html',
   styleUrls: ['./products.scss']
 })
