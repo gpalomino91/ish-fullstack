4843fc8642dd6537dc621861f30457831e3b9ee3
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-06-13 18:49:44 +0000

    Conectar ProductsComponent con ProductService usando Observable

--- frontend/ish-web/src/app/pages/products/products.ts
+++ frontend/ish-web/src/app/pages/products/products.ts
@@ -11,37 +11,6 @@ import { ProductCard } from '../../components/product-card/product-card';
   styleUrls: ['./products.scss']
 })
 export class Products {
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'NerdMiner',
-      price: 29.99,
-      description: 'Bitcoin Miner',
-      imageUrl: '',
-      category: 'Miners',
-      stock: 50,
-      rating: 5
-    },
-    {
-      id: 2,
-      name: 'Smart WiFi Light Switch',
-      price: 17.99,
-      description: 'Control your lights remotely with Alexa and Google Home support.',
-      imageUrl: '',
-      category: 'Smart Switches',
-      stock: 80,
-      rating: 4.5
-    },
-    {
-      id: 3,
-      name: 'Tuya Motion Sensor',
-      price: 12.50,
-      description: 'Infrared sensor for smart automation routines.',
-      imageUrl: '',
-      category: 'Sensors',
-      stock: 120,
-      rating: 4.7
-    }
-  ];
+  
 }
 
---
+++ frontend/ish-web/src/app/services/product.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { Observabl, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products: Product[] = [ {
+      id: 1,
+      name: 'NerdMiner',
+      price: 29.99,
+      description: 'Bitcoin Miner',
+      imageUrl: '',
+      category: 'Miners',
+      stock: 50,
+      rating: 5
+    },
+    {
+      id: 2,
+      name: 'Smart WiFi Light Switch',
+      price: 17.99,
+      description: 'Control your lights remotely with Alexa and Google Home support.',
+      imageUrl: '',
+      category: 'Smart Switches',
+      stock: 80,
+      rating: 4.5
+    },
+    {
+      id: 3,
+      name: 'Tuya Motion Sensor',
+      price: 12.50,
+      description: 'Infrared sensor for smart automation routines.',
+      imageUrl: '',
+      category: 'Sensors',
+      stock: 120,
+      rating: 4.7
+    } 
+  ];
+
+  getProducts(): Observable<Product[]> {
+    return of(this.products);
+  }
+}
