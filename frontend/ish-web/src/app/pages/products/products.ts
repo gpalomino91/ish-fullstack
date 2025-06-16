217c43594f3f79ba88007357d4ebc71f2e5e27a4
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-06-16 19:19:41 +0000

    Agregar enlace 'Ver más' en ProductCard y estilizarlo

--- frontend/ish-web/src/app/pages/products/products.ts
+++ frontend/ish-web/src/app/pages/products/products.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
 import { ProductCard } from '../../components/product-card/product-card';
+import { ProductService} from '../../services/product'
 
 @Component({
   selector: 'app-products',
@@ -10,7 +11,13 @@ import { ProductCard } from '../../components/product-card/product-card';
   templateUrl: './products.html',
   styleUrls: ['./products.scss']
 })
-export class Products {
-  
-}
 
+export class Products {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
